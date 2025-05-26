a9f477e9b78a6edbed82cc7e17a604d173b8d5ac
rattlznake <147215321+Rattlznake@users.noreply.github.com>
2025-05-26 09:54:48 -0400

    rename nextauth directory

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,7 @@ import GithubProvider from "next-auth/providers/github";
 // Ensure environment variables are set during runtime
 const githubId = process.env.GITHUB_ID || '';
 const githubSecret = process.env.GITHUB_SECRET || '';
+const jwtSecret = process.env.JWT_SECRET || '';
 
 // Create the NextAuth handler with configuration
 const handler = NextAuth({
@@ -16,7 +17,8 @@ const handler = NextAuth({
     // Add additional configuration if needed
     pages: {
         signIn: '/login'
-    }
+    },
+    secret: jwtSecret
 });
 
 export { handler as GET, handler as POST }
